b645696bb2c7ee25cb1a184c8600315f6e0a5b3f
laurabbity915 <laura_yuen915@outlook.com>
2023-01-23 17:40:52 +0000

    create get and set local storage functions

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,20 +1,33 @@
 function gameStart() {
-    document.getElementById('start-screen').hidden = true;
-    document.getElementById("questions").hidden = false;
-
+    document.getElementById('start-screen').style.display = 'none';
+    document.getElementById("questions").style.display = 'block';
+    getShuffle(questions);
     //document.getElementById
-    console.log(getRandom(questions));
+    let displayQuestions = getFromStorage('questions');
+    document.getElementById('question-title').innerText = displayQuestions[0].question;
 
 };
 //function for getting a random queation list 
-function getRandom(arr) {
+function getShuffle(arr) {
     let temp = [];
 
-    while(arr.length > 0){
+    while (arr.length > 0) {
         let index = Math.floor(Math.random() * arr.length);
         temp.push(arr[index]);
         arr.splice(index, 1);
     }
+    saveToStorage('questions', temp);
+}
 
-    return temp;
+function showQuestion(questions) {
+
+}
+
+
+function getFromStorage(key) {
+    return JSON.parse(localStorage.getItem(key));
+}
+
+function saveToStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
 }
