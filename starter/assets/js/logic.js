7e9bb450c9db7f331887c586aa8b7eb2942b5776
laurabbity915 <laura_yuen915@outlook.com>
2023-01-23 16:27:01 +0000

    question shuffle function

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,6 +1,20 @@
-function gameStart () {
-document.getElementById('start-screen').hidden = true;
-document.getElementById("questions").hidden = false;
-document.getElementById
+function gameStart() {
+    document.getElementById('start-screen').hidden = true;
+    document.getElementById("questions").hidden = false;
+
+    //document.getElementById
+    console.log(getRandom(questions));
 
 };
+//function for getting a random queation list 
+function getRandom(arr) {
+    let temp = [];
+
+    while(arr.length > 0){
+        let index = Math.floor(Math.random() * arr.length);
+        temp.push(arr[index]);
+        arr.splice(index, 1);
+    }
+
+    return temp;
+}
