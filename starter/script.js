3979f6a5e8e8b8533ada55de65d8d766d1f67205
laurabbity915 <laura_yuen915@outlook.com>
2023-01-23 00:29:54 +0000

    add question objects

---
+++ starter/script.js
@@ -0,0 +1,42 @@
+const question1 = {
+    question: "Inside which HTML element do we put the JavaScript? ",
+    answer1: "<js>"
+    answer2: "<script>"
+    answer3: "<javascript>"
+    answer4: "<scripting>"
+}
+
+const question2 = {
+    question: "What is the correct JavaScript syntax to change the content of the HTML element below?
+
+    <p id="demo">This is a demonstration.</p> ",
+    answer1: "document.getElementByName ("p").innerHTML = "Hello World!"; "
+    answer2: "document.getElement("o").innerHTML = "Hello World!"; "
+    answer3: "#demo.innerHTML = "Hello World!"; "
+    answer4: "document.getElementByld("demo").innerHTML = "Hello World!"; "
+}
+    
+const question3 = {
+    question: "Inside which HTML element do we put the JavaScript? ",
+    answer1: "the <head> section"
+    answer2: "both the <head> section and <body> section are correct"
+    answer3: "the <body> section"
+    }
+
+const question4 = {
+    question: "The external JavaScript file must contain the <script> tag.",
+    answer1: "true"
+    answer2: "false"
+    }
+
+const question5 = {
+    question: "How do you write "Hello World" in an alert box? ",
+    answer1: "<msgBox("Hello World")>"
+    answer2: "<msg("Hello World")>"
+    answer3: "<alert("Hello World")>"
+    answer4: "<alertBox("Hello World")>"
+}
+
+function gameStart {
+document.getElementById("start") = 
+}
