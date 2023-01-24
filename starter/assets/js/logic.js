9d789e09b3dc02f2eb000c25040947dcc55175b6
laurabbity915 <laura_yuen915@outlook.com>
2023-01-24 19:18:52 +0000

    implement qustion count down timer implement the question result add comment

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,67 +1,121 @@
 function gameStart() {
-    document.getElementById('start-screen').style.display = 'none';
-    document.getElementById("questions").style.display = 'block';
-    saveToStorage('stage', 0);
-    getShuffle(questions);
 
+    // hide the start page
+    document.getElementById('start-screen').style.display = 'none';
+    // unhide the questions page
+    document.getElementById("questions").style.display = 'block';
+
+    // init the game stage
+    saveToStorage('stage', 0);
+    // shuffle the questions
+    getShuffle(questions);
+    countdowntimer(10);
+    // display the questions
     showQuestion();
 };
-//function for getting a random queation list 
+//function for getting a random question list 
 function getShuffle(arr) {
     let temp = [];
 
     while (arr.length > 0) {
         let index = Math.floor(Math.random() * arr.length);
+        //add selected question to temp array
         temp.push(arr[index]);
+        //remove the selected question
         arr.splice(index, 1);
     }
+    // save the shuffled questions to local storage
     saveToStorage('questions', temp);
 }
 
+// function for render the question
 function showQuestion() {
     let displayQuestions = getFromStorage('questions');
     let choices = '';
     let stage = getFromStorage('stage');
-    if(stage < displayQuestions.length){
-        document.getElementById('question-title').innerText = displayQuestions[stage].question;
+
+    // show next question if the user hasn't answer all question, else to end-game page
+    if (stage < displayQuestions.length) {
+        document.getElementById('question-title').innerHTML = displayQuestions[stage].question;
         //console.log(displayQuestions);
         console.log(stage);
+        // create answer buttons
         for (let i = 0; i < displayQuestions[stage].choices.length; i++) {
             choices = choices.concat('<button onclick="checkAnswer(' + i + ')">' + displayQuestions[stage].choices[i] + ' </button>');
             //console.log(displayQuestions[stage].choices[i]);
         }
         //console.log(choices);
         document.getElementById('choices').innerHTML = choices;
-    }else {
+    } else {
         // TODO to end game
+        document.getElementById('end-screen').style.display = 'block';
+        document.getElementById("questions").style.display = 'none';
     }
 
 
 }
 
+// function for checking the user answer
 function checkAnswer(selectedIndex) {
     //console.log(selectedIndex);
     let displayQuestions = getFromStorage('questions');
     let stage = getFromStorage('stage');
 
-    console.log(displayQuestions[stage].answer);
-    console.log(displayQuestions[stage].choices[selectedIndex]);
+    // check the user answer
     if (displayQuestions[stage].answer === displayQuestions[stage].choices[selectedIndex]) {
-        console.log('correct');
-        //if correct, go to next stage
-        saveToStorage('stage', stage+1);
-        //show next question
-        showQuestion();
+        questionResult('you are correct');
+
     } else {
-        console.log('incorrect');
-        //TODO reduce time
+        questionResult('you are wrong');
+        //TODO subtract time
     }
+    //show next question
+    saveToStorage('stage', stage + 1);
+    showQuestion();
 }
 
+// function for geting local storage value
 function getFromStorage(key) {
     return JSON.parse(localStorage.getItem(key));
 }
 
+// function for saving local storage value
 function saveToStorage(key, value) {
     localStorage.setItem(key, JSON.stringify(value));
 }
+
+//function for showing result of each question
+function questionResult(result) {
+    // show the result message
+    document.getElementById('result').style.opacity = 1;
+    document.getElementById('result').innerText = result;
+    // hidden the message after 2 seconds 
+    setInterval(function () {
+        console.log('setInterval');
+        document.getElementById('result').style.opacity = 0;
+    }, 2000)
+}
+
+
+//countdown timer()
+function countdowntimer(seconds) {
+    let t = new Date();
+    let endTime = new Date(t.getTime() + ((seconds) * 1000));
+
+    const interval = setInterval(() => {
+        let current = new Date().getTime();
+        countdown = endTime - current;
+
+        document.getElementById('time').innerText =
+            Math.floor((countdown % (1000 * 60)) / 1000);
+
+        if (countdown <= 0) {
+            console.log('end countdown')
+            clearInterval(interval);
+            document.getElementById('time').innerText = 'END';
+        }
+    });
+
+};
+
+
